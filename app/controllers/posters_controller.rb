a6b25d1238745917e81f6f3e59b655722b81592d
gkurt <gkurt2017@gmail.com>
2017-08-12 07:57:36 +0000

    added haml

--- app/controllers/posters_controller.rb
+++ app/controllers/posters_controller.rb
@@ -1,5 +1,32 @@
 class PostersController < ApplicationController
+    before_action :find_poster, only: [:show, :edit, :update, :destroy]
+    
     def index
-        
     end
+    
+    def show
+    end
+    
+    def new
+        @poster = Poster.new
+    end
+    
+    def create
+        @poster = Poster.new(poster-params)
+        if @poster.save
+            redirect_to @poster, notice: "Posted!!!"
+        else
+            render 'new'
+        end
+    end    
+    
+    private
+    def poster_params
+        params.require(:poster).permit(:title, :description)
+    end
+    
+    def find_poster
+        @poster = Poster.new(params[:id])
+    end
+    
 end
